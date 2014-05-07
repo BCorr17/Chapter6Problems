d733ca41d366f8ab09c61dc6f3155bb662422ef0
BCorr17 <CorrBradley@gmail.com>
2014-05-07 10:06:17 -0400

    vGSFSDFGgfger

---
+++ Chapter6Problems/InchesToFeet.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InchesToFeet.cs
+{
+    class Program
+    {
+        public static void InchesToFeet(int inches, 
+        {
+
+        }
+    }
+}
