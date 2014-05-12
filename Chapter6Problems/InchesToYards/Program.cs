1c9fc3153c9067856e28acdd6beed8a9cbc20a1c
BCorr17 <CorrBradley@gmail.com>
2014-05-12 09:35:58 -0400

    679eis75isis57

---
+++ Chapter6Problems/InchesToYards/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InchesToFeet.cs
+{
+    class Program
+    {
+        public static void InchesToYards(int inches)
+        {
+            int feet, inch, yard;
+
+            feet = inches / 12;
+            inch = inches % 12;
+            yard = feet / 3;
+
+            Console.WriteLine("This equals {0} yard  {1} feet and {2} Inches", yard, feet, inch);
+        }
+
+        static void Main(string[] args)
+        {
+            int inches = 67;
+            InchesToYards(inches);
+        }
+    }
+}
