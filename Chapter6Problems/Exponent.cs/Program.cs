5f7c5c514018d901c9ea29aa9decc2cccc8b930f
BCorr17 <CorrBradley@gmail.com>
2014-05-13 10:05:48 -0400

    6suaurt

---
+++ Chapter6Problems/Exponent.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exponent.cs
+{
+    class Program
+    {
+        public static void Exponent(int value)
+        {
+            string input;
+        }
+
+        public static void Square
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
